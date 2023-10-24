62b4733f3d307f4f22faa14947c5776a9296a32e
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2023-10-24 20:11:51 -0300

    segmentacao: Adiciona codigo simples de segmentacao com a tecnica de Limiarizacao, opencv e python.

---
+++ exemplos/limiarizacao.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+path_img = 'exemplos/dados/imagens/paisagem01.jpg'
+
+img_BGR = cv2.imread(path_img)
+img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+img_GRAY = cv2.cvtColor(img_RGB, cv2.COLOR_RGB2GRAY)
+
+limiar = 140
+
+val_limiar, thresh = cv2.threshold(img_GRAY, limiar, 255, cv2.THRESH_BINARY)
+
+fig = plt.gcf()
+fig.set_size_inches(18, 6)
+
+plt.imshow(thresh, cmap = 'gray')
+plt.axis('off')
+plt.show()
+
+# cv2.imwrite('resultado.jpg', thresh)
